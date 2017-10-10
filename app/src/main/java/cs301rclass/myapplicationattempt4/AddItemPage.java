f33dad509e94da3c07986158077df99722ccae6e
KasiKarra <hate2shop@sbcglobal.net>
2017-10-10 16:28:52 -0600

    10-10-2017 a
    login mostly finished.  AddItem Page added.

---
+++ app/src/main/java/cs301rclass/myapplicationattempt4/AddItemPage.java
@@ -0,0 +1,13 @@
+package cs301rclass.myapplicationattempt4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddItemPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item_page);
+    }
+}
--- app/src/main/java/cs301rclass/myapplicationattempt4/Login.java
+++ app/src/main/java/cs301rclass/myapplicationattempt4/Login.java
@@ -2,13 +2,8 @@ package cs301rclass.myapplicationattempt4;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,8 +22,10 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         login  = (Button) findViewById(R.id.login);
+
         username  = (EditText) findViewById(R.id.email);
         password  = (EditText) findViewById(R.id.password);
+
         note =  (TextView) findViewById(R.id.note);
 
 
@@ -38,37 +35,26 @@ public class Login extends AppCompatActivity {
              if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
                  switchActivities();
                  //note.setText("awesome");
-             else
-                 note.setText("Try Again");
+             else {
+                 note.setText("Incorrect Username or Password");
+                 password.setText("");
+             }
           }
          });
     }
 
     public void switchActivities()
     {
+        //clear all fields
+        note.setText("");
+        username.setText("");
+        password.setText("");
+
+        //move to the dataPage
         Intent intent = new Intent(this, dataPage.class);
         startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
