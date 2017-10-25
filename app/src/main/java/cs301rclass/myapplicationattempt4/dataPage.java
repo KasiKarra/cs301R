f780644f12bc62544b48f623bc4ea212e6b5e6b0
KasiKarra <hate2shop@sbcglobal.net>
2017-10-25 17:08:01 -0600

    datapage reformating
    changes to datapage structure

--- app/src/main/java/cs301rclass/myapplicationattempt4/dataPage.java
+++ app/src/main/java/cs301rclass/myapplicationattempt4/dataPage.java
@@ -1,3 +1,7 @@
+//getting left vs right swipes stackoverflow answer
+//https://stackoverflow.com/questions/4139288/android-how-to-handle-right-to-left-swipe-gestures
+
+
 package cs301rclass.myapplicationattempt4;
 
 import android.content.Context;
@@ -8,16 +12,18 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
@@ -25,37 +31,28 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.List;
 
 enum sortingType {ALPHABETICAL, CATEGORICAL, EXPIRATION}
 
-public class dataPage extends AppCompatActivity implements View.OnClickListener {
+public class dataPage extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
 
     private List<Food> foodList = new ArrayList<Food>();
-    private boolean googleSignIn = false;
-    private GoogleApiClient mGoogleApiClient;
-    private FloatingActionButton fab;
     private Button popupSelect;
     private RadioButton A, C, E;  //alphabetical, categorical, expiration
     private PopupWindow mypopup;
     private sortingType type = sortingType.ALPHABETICAL;
     private FirebaseAuth auth;
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference ref = database.getReference("server/saving-data/fireblog");
-
+    private static GestureDetector gd;
+    private ListView list;
+    ArrayAdapter<Food> adapter;
+    private TextView sortingTypeTextBox;
+    private Button RecipeViewer;
+    private ImageButton Sort, AddItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,55 +60,73 @@ public class dataPage extends AppCompatActivity implements View.OnClickListener 
         setContentView(R.layout.activity_data_page);
         Toolbar tools = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(tools);
-        populateFoodList();
-        populateListView();
-        registerClickCallback();
         auth = FirebaseAuth.getInstance();
-        fab = (FloatingActionButton) findViewById(R.id.add_button);
-        fab.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                addItemSwitch();
-            }
-        });
 
-  //      fba = FirebaseAuth.getInstance();
-  //      FirebaseUser user = fba.getCurrentUser();
-        //UID = user.getUid();
+        RecipeViewer = (Button) findViewById (R.id.RecipeButton);
+        RecipeViewer.setOnClickListener(this);
+        Sort = (ImageButton) findViewById(R.id.sort);
+        Sort.setOnClickListener(this);
+        AddItem = (ImageButton) findViewById(R.id.AddItemButton);
+        AddItem.setOnClickListener(this);
+
+        adapter = new dataPage.MyListAdapter();
+        list = (ListView) findViewById(R.id.RecipeList);
+        list.setAdapter(adapter);
+        populateFoodList();
+        registerClickCallback();
+
+        gd = new GestureDetector(this.getApplicationContext(), new GestureListener());
 
     }
 
-    @Override
-    public void onClick(View view)
-    {
-        A.setChecked(false);
-        C.setChecked(false);
-        E.setChecked(false);
 
-        switch(view.getId())
-        {
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.Alphabet:
+                A.setChecked(false);
+                C.setChecked(false);
+                E.setChecked(false);
                 type = sortingType.ALPHABETICAL;
                 A.setChecked(true);
                 break;
             case R.id.Category:
+                A.setChecked(false);
+                C.setChecked(false);
+                E.setChecked(false);
                 type = sortingType.CATEGORICAL;
                 C.setChecked(true);
                 break;
             case R.id.Expiration:
+                A.setChecked(false);
+                C.setChecked(false);
+                E.setChecked(false);
                 E.setChecked(true);
                 type = sortingType.EXPIRATION;
                 break;
             case R.id.Select:
                 mypopup.dismiss();
+
+                //change sorted text to reflect what is chosen
+                if(type == sortingType.ALPHABETICAL)
+                    sortingTypeTextBox.setText("Sorted Alphabetically");
+                else if(type == sortingType.CATEGORICAL)
+                    sortingTypeTextBox.setText("Sorted Categorically");
+                else
+                    sortingTypeTextBox.setText("Sorted by Expiration Date");
+
+                break;
+            case R.id.sort:
+                ShowPopupWindow();
+                break;
+            case R.id.RecipeButton:
+                //chaos insues . . . ?
+                break;
+            case R.id.AddItemButton:
+                Intent intent = new Intent(this, AddItemPage.class);
+                startActivity(intent);
                 break;
         }
-    }
-
-
-    public void addItemSwitch()
-    {
-        Intent intent = new Intent(this, AddItemPage.class);
-        startActivity(intent);
     }
 
     @Override
@@ -123,74 +138,66 @@ public class dataPage extends AppCompatActivity implements View.OnClickListener 
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-    //noinspection SimplifiableIfStatement
+        //noinspection SimplifiableIfStatement
         //if (id == R.id.action_settings) {
         //    return true;
         //}
-        if(id == R.id.Logoff)
-        {
+        if (id == R.id.Logoff) {
             auth.signOut();
-            Intent intent = new Intent(this,Login.class);
-            startActivity(intent);
+            finish();  //can we use finish instead of start new activity?
         }
-        if(id == R.id.Sort)
-            ShowPopupWindow();
         if(id == R.id.RecipeIdeas)
         {
             Intent intent = new Intent(this, RecipeViewer.class);
             startActivity(intent);
         }
-        if(id == R.id.FoodTrivia)
-        {
-            Intent intent = new Intent(this, FoodTrivia.class);
-            startActivity(intent);
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
     private void populateFoodList() {
-        foodList.add(new Food("Banana",5,Color.GREEN,R.drawable.fruit,"10-24-2017",FoodCategory.FRUIT));
-        foodList.add(new Food("apples",1,Color.RED,R.drawable.fruit,"10-24-2017",FoodCategory.FRUIT));
-        foodList.add(new Food("curry",3,Color.ORANGE,R.drawable.meat,"10-24-2017",FoodCategory.MEAT));
-        foodList.add(new Food("swedish fish",5,Color.GREEN,R.drawable.sweet,"10-24-2017",FoodCategory.SWEET));
-        foodList.add(new Food("sweetpotatoes",5,Color.GREEN,R.drawable.veggie,"10-24-2017",FoodCategory.FRUIT));
-        foodList.add(new Food("chicken",5,Color.GREEN,R.drawable.meat,"10-24-2017",FoodCategory.MEAT));
-        foodList.add(new Food("mochi",5,Color.GREEN,R.drawable.sweet,"10-24-2017",FoodCategory.SWEET));
-        foodList.add(new Food("chicken sandwiches",5,Color.GREEN,R.drawable.meat,"10-24-2017",FoodCategory.MEAT));
+        foodList.add(new Food("Banana", 5, Color.GREEN, R.drawable.fruit, "10-24-2017", FoodCategory.FRUIT));
+        foodList.add(new Food("apples", 1, Color.RED, R.drawable.fruit, "10-24-2017", FoodCategory.FRUIT));
+        foodList.add(new Food("curry", 3, Color.ORANGE, R.drawable.meat, "10-24-2017", FoodCategory.MEAT));
+        foodList.add(new Food("swedish fish", 5, Color.GREEN, R.drawable.sweet, "10-24-2017", FoodCategory.SWEET));
+        foodList.add(new Food("sweetpotatoes", 5, Color.GREEN, R.drawable.veggie, "10-24-2017", FoodCategory.FRUIT));
+        foodList.add(new Food("chicken", 5, Color.GREEN, R.drawable.meat, "10-24-2017", FoodCategory.MEAT));
+        foodList.add(new Food("mochi", 5, Color.GREEN, R.drawable.sweet, "10-24-2017", FoodCategory.SWEET));
+        foodList.add(new Food("chicken sandwiches", 5, Color.GREEN, R.drawable.meat, "10-24-2017", FoodCategory.MEAT));
+        foodList.add(new Food("chicken sandwiches", 5, Color.GREEN, R.drawable.meat, "10-24-2017", FoodCategory.MEAT));
+        foodList.add(new Food("chicken sandwiches", 5, Color.GREEN, R.drawable.meat, "10-24-2017", FoodCategory.MEAT));
+        foodList.add(new Food("chicken sandwiches", 5, Color.GREEN, R.drawable.meat, "10-24-2017", FoodCategory.MEAT));
+        foodList.add(new Food("chicken sandwiches", 5, Color.GREEN, R.drawable.meat, "10-24-2017", FoodCategory.MEAT));
+        foodList.add(new Food("chicken sandwiches", 5, Color.GREEN, R.drawable.meat, "10-24-2017", FoodCategory.MEAT));
+        foodList.add(new Food("chicken sandwiches", 5, Color.GREEN, R.drawable.meat, "10-24-2017", FoodCategory.MEAT));
     }
 
-    private void populateListView() {
-        ArrayAdapter<Food> adapter = new dataPage.MyListAdapter();
-        ListView list = (ListView)findViewById(R.id.RecipeList);
-        list.setAdapter(adapter);
-    }
     private void registerClickCallback() {
-        ListView list = (ListView)findViewById(R.id.RecipeList);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View viewClicked,int position, long id){
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
                 Food clickedFood = foodList.get(position);
                 String message = "you clicked position " + position + " which is the food " + clickedFood.getName();
-                Toast.makeText(dataPage.this,message,Toast.LENGTH_LONG).show();
+                Toast.makeText(dataPage.this, message, Toast.LENGTH_LONG).show();
             }
         });
     }
-    private class MyListAdapter extends ArrayAdapter<Food>{
-        public MyListAdapter(){
-            super(dataPage.this,R.layout.item_view,foodList);
+
+    private class MyListAdapter extends ArrayAdapter<Food> {
+        public MyListAdapter() {
+            super(dataPage.this, R.layout.item_view, foodList);
         }
+
         @Override
-        public View getView(int position, View convertView, ViewGroup parent){
+        public View getView(int position, View convertView, ViewGroup parent) {
             //make sure we have a view to work with(may be given null)
             View itemView = convertView;
-            if(itemView == null){
-                itemView = getLayoutInflater().inflate(R.layout.item_view,parent,false);
+            if (itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.item_view, parent, false);
             }
             //find the food
             Food currFood = foodList.get(position);
@@ -200,7 +207,7 @@ public class dataPage extends AppCompatActivity implements View.OnClickListener 
             //name
             TextView counterText = (TextView) itemView.findViewById(R.id.countdown);
             counterText.setTextColor(getCurrFoodColor(currFood));
-            counterText.setText(""+currFood.getDaysUntilExp());
+            counterText.setText("" + currFood.getDaysUntilExp());
 
             TextView dateText = (TextView) itemView.findViewById(R.id.item_expDate);
             dateText.setText(currFood.getExpDate());
@@ -213,15 +220,14 @@ public class dataPage extends AppCompatActivity implements View.OnClickListener 
         }
 
 
-
-        int getCurrFoodColor(Food currFood){
+        int getCurrFoodColor(Food currFood) {
             int color = 0;
-            switch(currFood.getColor()){
+            switch (currFood.getColor()) {
                 case GREEN:
-                    color =  -16711936;
+                    color = -16711936;
                     break;
                 case ORANGE:
-                    color = android.graphics.Color.rgb(255,165,0);
+                    color = android.graphics.Color.rgb(255, 165, 0);
                     break;
                 case RED:
                     color = -65536;
@@ -232,8 +238,7 @@ public class dataPage extends AppCompatActivity implements View.OnClickListener 
         }
     }
 
-
-    // this listener will be called when there is change in firebase user session
+	    // this listener will be called when there is change in firebase user session
     FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
         @Override
         public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -246,10 +251,11 @@ public class dataPage extends AppCompatActivity implements View.OnClickListener 
             }
         }
     };
+	
     //create the sorting popup window
-    private void ShowPopupWindow(){
+    private void ShowPopupWindow() {
         try {
-            ImageView btncall, btnsound, btncamera, btnvideo,btngallary,btnwrite;
+            ImageView btncall, btnsound, btncamera, btnvideo, btngallary, btnwrite;
             LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View layout = inflater.inflate(R.layout.fragment_sortingtypes, null);
             mypopup = new PopupWindow(layout, 750, 500, true);
@@ -268,9 +274,9 @@ public class dataPage extends AppCompatActivity implements View.OnClickListener 
             C.setOnClickListener(this);
             E.setOnClickListener(this);
 
-            if(type == sortingType.ALPHABETICAL)
+            if (type == sortingType.ALPHABETICAL)
                 A.setChecked(true);
-            else if(type == sortingType.CATEGORICAL)
+            else if (type == sortingType.CATEGORICAL)
                 C.setChecked(true);
             else
                 E.setChecked(true);
@@ -278,6 +284,64 @@ public class dataPage extends AppCompatActivity implements View.OnClickListener 
             popupSelect = (Button) layout.findViewById(R.id.Select);
             popupSelect.setOnClickListener(this);
 
-        }catch (Exception e){}
+        } catch (Exception e) {
+        }
+    }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return gd.onTouchEvent(event);
+    }
+
+    private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            try {
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight();
+                        } else {
+                            onSwipeLeft();
+                        }
+                        result = true;
+                    }
+                } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeBottom();
+                    } else {
+                        onSwipeTop();
+                    }
+                    result = true;
+                }
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            return result;
+        }
+
+        public void onSwipeRight() {
+        }
+
+        public void onSwipeLeft() {
+        }
+
+        public void onSwipeTop() {
+        }
+
+        public void onSwipeBottom() {
+        }
     }
 }
